a3891ab1b239354994ff8891f2da02ddd6544a51
Peter Gazdik <peter.gazdik@gmail.com>
2024-02-14 22:14:05 +0100

    Model template no longer adds request-api-model

--- model-priming-template/dependencies.groovy
+++ model-priming-template/dependencies.groovy
@@ -7,28 +7,15 @@ import com.braintribe.devrock.templates.model.Property;
 def buildSystemConfigRequest = CreateBuildSystemConfig.T.create();
 support.mapFromTo(request, buildSystemConfigRequest);
 buildSystemConfigRequest.artifactType = 'model';
-def dep1 = Dependency.T.create();
-dep1.groupId = 'com.braintribe.gm';
-dep1.artifactId = 'root-model';
-dep1.tags = ['asset'];
-def deps = [dep1];
-if (request.api) {
-	def dep2 = Dependency.T.create();
-	dep2.groupId = 'com.braintribe.gm'
-	dep2.artifactId = 'service-api-model'
-	dep2.tags = ['asset'];
-	def dep3 = Dependency.T.create();
-	dep3.groupId = 'com.braintribe.gm'
-	dep3.artifactId = 'access-request-model'
-	dep3.tags = ['asset'];
-	deps += [dep2, dep3];
-} else if (request.deployment) {
-	def dep2 = Dependency.T.create();
-	dep2.groupId = 'tribefire.cortex'
-	dep2.artifactId = 'extension-deployment-model'
-	dep2.tags = ['asset'];
-	deps += [dep2];
-}
+
+def deps = [assetDependency('com.braintribe.gm', 'root-model')];
+
+if (request.api)
+	deps += [assetDependency('com.braintribe.gm', 'service-api-model')];
+
+if (request.deployment)
+	deps += [assetDependency('tribefire.cortex', 'extension-deployment-model')];
+
 buildSystemConfigRequest.dependencies = support.distinctDependencies(deps + buildSystemConfigRequest.dependencies);
 def archetypeProp = Property.T.create();
 archetypeProp.name = 'archetype';
@@ -41,7 +28,11 @@ projectMetadataRequest.projectName = request.artifactId + " - " + request.groupI
 projectMetadataRequest.sourceDirectory = 'src';
 if ('eclipse'.equals(request.ide)) {
 	projectMetadataRequest.classPathEntries = ["org.eclipse.jdt.launching.JRE_CONTAINER"];
-	projectMetadataRequest.builders = ['com.braintribe.devrock.arb.builder.ArtifactReflectionBuilder', 'org.eclipse.jdt.core.javabuilder', 'com.braintribe.eclipse.model.nature.ModelBuilder'];
+	projectMetadataRequest.builders = [
+		'com.braintribe.devrock.arb.builder.ArtifactReflectionBuilder',
+		'org.eclipse.jdt.core.javabuilder',
+		'com.braintribe.eclipse.model.nature.ModelBuilder'
+	];
 	projectMetadataRequest.builderOutputLibs = ["class-gen"];
 	projectMetadataRequest.natures = ['org.eclipse.jdt.core.javanature', 'com.braintribe.eclipse.model.nature.ModelNature'];
 }
@@ -50,4 +41,12 @@ def sourceControlConfigRequest = CreateSourceControlConfig.T.create();
 support.mapFromTo(request, sourceControlConfigRequest);
 sourceControlConfigRequest.ignoredFiles = ['/' + projectMetadataRequest.outputDirectory];
 
-[buildSystemConfigRequest, projectMetadataRequest, sourceControlConfigRequest]
+return [buildSystemConfigRequest, projectMetadataRequest, sourceControlConfigRequest]
+
+Dependency assetDependency(String groupId, String artifactId) {
+	def dep = Dependency.T.create();
+	dep.groupId = groupId;
+	dep.artifactId = artifactId;
+	dep.tags = ['asset'];
+	return dep;
+}
